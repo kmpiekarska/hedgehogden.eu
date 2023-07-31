f8344f0f110b5f0de52977e0cb464791177d24e6
Katarzyna Piekarska <kmpiekarska@gmail.com>
2023-07-31 15:21:53 +0200

    Updated image urls

---
+++ src/assets/index.ts
@@ -0,0 +1,29 @@
+import Bernie from "/Bernie.png";
+import Bianca from "/Bianca.png";
+import Charlie from "/Charlie.png";
+import GrandmaGnome from "/GrandmaGnome .png";
+import GrandpaGnome from "/GrandpaGnome.png";
+import Greta from "/Greta.png";
+import KimFrog from "/KimFrog.png";
+import Lily from "/Lily.png";
+import MoonBear from "/MoonBear.png";
+import MrsHen from "/MrsHen.png";
+import Rudolphine from "/Rudolphine.png";
+import SantaGnome from "/SantaGnome.png";
+import Velvet from "/Velvet.png";
+
+export {
+  Bernie,
+  Bianca,
+  Charlie,
+  GrandmaGnome,
+  GrandpaGnome,
+  Greta,
+  KimFrog,
+  Lily,
+  MoonBear,
+  MrsHen,
+  Rudolphine,
+  SantaGnome,
+  Velvet,
+};
